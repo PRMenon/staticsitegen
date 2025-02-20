02162216bebf6038de223f28f635d49576102150
PRMenon <pratheek.menon@gmail.com>
2025-02-20 05:30:43 +0000

    added htmlnodes and text_to_html functions

---
+++ src/htmlnode.py
@@ -0,0 +1,49 @@
+class HTMLNode():
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("Child classes must override this")
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+        
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value")
+        if not self.tag:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        return super().__repr__()
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Tag is required for Parent node")
+        if not self.children:
+            raise ValueError("Children are required for Parent node")
+        text = "".join((node.to_html() for node in self.children))
+        return f"<{self.tag}>{text}</{self.tag}>"
+    
+    def __repr__(self):
+        return super().__repr__()
