5fef7ea8d584e7207af6b1ae45adf65586dbe637
PRMenon <pratheek.menon@gmail.com>
2025-02-22 06:24:02 +0000

    added link and image parsing functionality

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -26,7 +26,50 @@ def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 def split_nodes_image(old_nodes: list) -> list:
-    pass
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        images = extract_markdown_images(node.text)
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+
+        for image in images:
+            alt, link = image
+            image_node = TextNode(alt, TextType.IMAGE, link)
+            test_without_image = node.text.split(f"![{alt}]({link})")
+            if test_without_image[0]:
+                new_nodes.append(TextNode(test_without_image[0], TextType.TEXT))
+            new_nodes.append(image_node)
+            node.text = test_without_image[1]
+        if node.text:
+            new_nodes.append(TextNode(node.text, TextType.TEXT))
+    return new_nodes
+
 
 def split_nodes_link(old_nodes: list) -> list:
-    pass
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        links = extract_markdown_links(node.text)
+        if len(links) == 0:
+            new_nodes.append(node)
+            continue
+        
+        for link_ in links:
+            alt, link = link_
+            link_node = TextNode(alt, TextType.LINK, link)
+            test_without_link_ = node.text.split(f"[{alt}]({link})")
+            if test_without_link_[0]:
+                new_nodes.append(TextNode(test_without_link_[0], TextType.TEXT))
+            new_nodes.append(link_node)
+            node.text = test_without_link_[1]
+        if node.text:
+            new_nodes.append(TextNode(node.text, TextType.TEXT))
+    return new_nodes
